5257aaa1feb6389e44758ab1375970e265dfe1aa
cephalopodMD <mistahowe@gmail.com>
2016-05-28 01:37:52 -0400

    potterish initial commit

---
+++ finite-incantatum.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+shopt -s expand_aliases
+
+unalias alohomora
+unalias aparecium
+unalias apparate
+unalias avada-kedavra
+unalias depulso
+unalias diminuendo
+unalias engorgio
+unalias epoximize
+unalias finite-incantatum
+unalias gemino
+unalias homenum-revelio
+unalias imperio
+unalias legilimens
+unalias locomotor
+unalias lumos
+unalias lumos-solem
+unalias obliviate
+unalias petrificus-totalis
+unalias portus
+unalias prior-incantato
+unalias reducio
+unalias reducto
+unalias rennervate
+unalias specialis-revelio
+
+echo 'muggle...'
+echo ''
---
+++ potterish.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+shopt -s expand_aliases
+
+#accio
+#    finds/searches for the thing you were looking for
+alias alohomora=chmod
+alias aparecium='ls -a'
+alias apparate=cd
+#arresto-momentum
+#    lower process priority?
+alias avada-kedavra=pkill
+alias depulso=mv
+alias diminuendo=zip
+alias engorgio=unzip
+alias epoximize=cat
+#evanesco
+#    prepend a . to the filename
+#fidelius
+#    encrypts a file
+alias finite-incantatum='source ./finite-incantatum.sh'
+alias gemino=cp
+alias homenum-revelio=finger
+alias imperio=sudo
+#horcrux curse
+#    infects a file with a virus (lol)
+#langlock
+#    redirects stdout of a process to /dev/null
+alias legilimens=gdb
+alias locomotor=mv
+alias lumos=ls
+alias lumos-solem='ls -al'
+alias obliviate=clear
+alias petrificus-totalis='pkill -TSTP'
+alias portus=ln
+alias prior-incantato=history
+alias reducio=zip
+alias reducto=rm
+alias rennervate='pkill -CONT'
+#silencio
+#    redirects stdout of a process to /dev/null
+alias specialis-revelio=file
+
+echo 'Welcome to potteriSH!'
+echo ''
